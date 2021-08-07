c49716c7773f8daf6fc058faef67c17ee6a0f520
Phillip Chu <misterpchu@gmail.com>
2021-08-07 15:45:12 -0700

    Made changes to config

--- flask_blog/config.py
+++ flask_blog/config.py
@@ -16,4 +16,3 @@ class Config:
     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
     MAIL_USE_TLS = os.getenv("MAIL_USE_TLS")
-    MAIL_USE_SSL = os.getenv("MAIL_USE_SSL")
